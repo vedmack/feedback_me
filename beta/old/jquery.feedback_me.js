f207134b717eede4f179ed5255f8b3e33dff8be0
Daniel <vedmack@gmail.com>
2014-09-07 22:37:51 +0300

    0.5.7.beta.3 - IE8 support (diff' impl')
    https://github.com/vedmack/feedback_me/issues/28

--- beta/old/jquery.feedback_me.js
+++ beta/old/jquery.feedback_me.js
@@ -5,7 +5,7 @@
 * jQuery Feedback Me Plugin
 * 
 * File:			jquery.feedback_me.js
-* Version:		0.5.7.beta.3
+* Version:		0.5.7.beta2
 * 
 * Author:      Daniel Reznick
 * Info:        https://github.com/vedmack/feedback_me 
@@ -221,21 +221,17 @@ var fm = (function ($) {
 	var fm_options_arr = {},
 		supportsTransitions = false;
 
-	function eventTargetFixUp(pEvent) {
-		if (pEvent.target === undefined) {
-            pEvent.target = pEvent.srcElement;
-        }
-		return pEvent;
+	function getEventTarget(event) {
+		return event.target || event.srcElement;
 	}
 
 	function getFmOptions(event, position) {
 		var className,
 			selector;
-		event = eventTargetFixUp(event);
-		if ($(event).closest(".feedback_trigger").length === 1) {
-			className = $(event).closest(".feedback_trigger")[0].className;
-		} else if ($(event).closest(".feedback_content").length === 1) {
-			className = $(event).closest(".feedback_content")[0].className;
+		if ($(getEventTarget(event)).closest(".feedback_trigger").length === 1) {
+			className = $(getEventTarget(event)).closest(".feedback_trigger")[0].className;
+		} else if ($(getEventTarget(event)).closest(".feedback_content").length === 1) {
+			className = $(getEventTarget(event)).closest(".feedback_content")[0].className;
 		} else {
 			if (position === undefined) {
 				position = 'left-top';
@@ -262,7 +258,6 @@ var fm = (function ($) {
 			$fm_trigger,
 			$fm_content;
 
-		event = eventTargetFixUp(event);
 		animation_show.marginLeft = "+=380px";
 		animation_hide.marginLeft = "-=380px";
 
@@ -271,11 +266,11 @@ var fm = (function ($) {
 			animation_hide.marginRight = "-=380px";
 		}
 
-		$fm_trigger = $(event).closest(".feedback_trigger");
+		$fm_trigger = $(getEventTarget(event)).closest(".feedback_trigger");
 		if ($fm_trigger.length === 1) {
 			$fm_content = $fm_trigger.next();
 		} else {
-			$fm_content = $(event).closest(".feedback_content");
+			$fm_content = $(getEventTarget(event)).closest(".feedback_content");
 			$fm_trigger = $fm_content.prev();
 		}
 		if ($fm_content.length === 0 || $fm_trigger.length === 0) {
@@ -314,9 +309,9 @@ var fm = (function ($) {
 	}
 
 	function closeFeedback(event) {
-		event = eventTargetFixUp(event);
+
 		if (($(".feedback_content").length === 1 && $(".feedback_content").hasClass("feedback_content_closed")) ||
-				$(event).closest('.feedback_content').length === 1) {
+				$(getEventTarget(event)).closest('.feedback_content').length === 1) {
 			return;
 		}
 
@@ -369,8 +364,7 @@ var fm = (function ($) {
 	}
 
 	function validateFeedbackForm(event, position) {
-		event = eventTargetFixUp(event);
-		var $fm_content = $(event).closest(".feedback_content"),
+		var $fm_content = $(getEventTarget(event)).closest(".feedback_content"),
 			fm_options = getFmOptions(event, position);
 		if ((fm_options.name_required === true && $fm_content.find(".feedback_name").val() === "") ||
 				((fm_options.email_required === true && $fm_content.find(".feedback_email").val() === "") || (fm_options.email_required === true && emailValid($fm_content.find(".feedback_email").val()) === false)) ||
@@ -587,8 +581,7 @@ var fm = (function ($) {
 		}
 	}
 	function clearInputs(event) {
-		event = eventTargetFixUp(event);
-		var $fm_content = $(event).closest(".feedback_content");
+		var $fm_content = $(getEventTarget(event)).closest(".feedback_content");
 
 		$fm_content.find(".feedback_name").val("");
 		$fm_content.find(".feedback_message").val("");
@@ -604,14 +597,13 @@ var fm = (function ($) {
 			$fm_content,
 			fm_options = getFmOptions(event, position);
 
-		event = eventTargetFixUp(event);
 		if (checkValid === false || checkPattern === false) {
 			stopPropagation(event);
 			return;
 		}
 
-		$fm_content = $(event).closest(".feedback_content");
-		$fm_trigger = $(event).closest(".feedback_content").prev();
+		$fm_content = $(getEventTarget(event)).closest(".feedback_content");
+		$fm_trigger = $(getEventTarget(event)).closest(".feedback_content").prev();
 
 		if (fm_options.delayed_close === true) {
 			$fm_content.find('.feedback_submit').text(fm_options.delayed_options.sending);
